d802bda9b740ac15451546cc7a35ee4b6ed88b9a
ARUMHC <56126275+ARUMHC@users.noreply.github.com>
2021-01-12 19:12:06 +0100

    Add histogram in charts.js

--- js/charts/histogram.js
+++ js/charts/histogram.js
@@ -1,17 +1,108 @@
 //const d3 = require("jquery");
 
 function updateHistogram(dataInput) {
+
+    var dataX = dataInput.map(v => v[SELECTED_DATATYPE]);
+    dataX.sort(function(a, b){return b-a});
+    //console.log("DATA X")
+    //console.log(dataX);
+
+    min = d3.min(dataX);
+    max = d3.max(dataX);
+    domain = [min,max];
+    Nbin = 10; // The number of bins
+
+    step = (max - min)/Nbin;
+    buckets = [];
+    labels = [];
+    bucketborder = max;
+    labels.unshift(Math.round(bucketborder));
+    bucketsize = 0;
+
+    for (let i = 0; i < dataX.length ; i++){
+        if (dataX[i] >= bucketborder){
+            bucketsize += 1;
+        }
+        else{
+            if (bucketsize != 0 || bucketborder > 1){
+                buckets.unshift(bucketsize);
+                labels.unshift(Math.round(bucketborder));
+            }
+            //buckets.unshift(bucketsize);
+            bucketsize = 0;
+            bucketborder = bucketborder/2;
+            //labels.unshift(Math.round(bucketborder));
+            //console.log(bucketborder);
+            //i = i - 1;
+        }
+        //console.log("kupa");
+    }
+    buckets.unshift(bucketsize);
+    console.log("BUCKET");
+    console.log(buckets);
+    //console.log(buckets.length);
+    console.log("LABELS");
+    console.log(labels);
+    //console.log(labels.length);
+//end of data processing
+
+    if(SELECTED_DATATYPE == "General") {
+        $('#histogramTitle').text(" ")
+    } else {
+        $('#histogramTitle').text("Histogram of " +SELECTED_DATATYPE.toLowerCase()+" over time")
+    }
+
+    const ctx = document.getElementById('histogram');
+
+    const histogram = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Number of blah blah',
+                data: buckets,
+                backgroundColor: '#C34A36',
+            }]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    display: false,
+                    barPercentage: 1.3,
+                    ticks: {
+                        max: 3,
+                    }
+                }, {
+                    display: true,
+                    ticks: {
+                        autoSkip: false,
+                        max: 4,
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+
+    /*
     d3.select("#histogram>svg").remove();
 
     var dataX = dataInput.map(v => v[SELECTED_DATATYPE]);
 
+    console.log("BLAHBLAH");
+
     min = d3.min(dataX);
-    //console.log("M I N");
-    //console.log(min);
     max = d3.max(dataX);
-    //console.log("M A X");
-    //console.log(max);
     domain = [min,max];
+    Nbin = 10; // The number of bins
+
 
     if(SELECTED_DATATYPE == "General") {
         $('#histogramTitle').text(" ")
@@ -24,7 +115,8 @@ function updateHistogram(dataInput) {
         height = 400 - margin.top - margin.bottom;
 
 // The number of bins
-    Nbin = 10;
+
+
 
     var x = d3.scaleLinear()
         .domain(domain)
@@ -86,5 +178,7 @@ function updateHistogram(dataInput) {
             return height - y(d.length);
         })
         .style("fill", "#C34A36");
+
+     */
 
 }
