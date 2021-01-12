c63517bd0001d1816dd1dd00f17035077091295d
adrianstando <56126191+adrianstando@users.noreply.github.com>
2021-01-12 12:43:25 +0100

    Change mean to median in d3 data processing

--- js/charts/heatMap.js
+++ js/charts/heatMap.js
@@ -114,7 +114,7 @@ function updateHeatMap(dataInput) {
       })
       
   if(SELECTED_DATATYPE == "General") dataX = dataX.rollup(v => v.length)
-  else dataX = dataX.rollup(v => d3.mean(v, function(d) { return +d[SELECTED_DATATYPE]; }))
+  else dataX = dataX.rollup(v => d3.median(v, function(d) { return +d[SELECTED_DATATYPE]; }))
       
   dataX = dataX.entries(dataInput);
 
