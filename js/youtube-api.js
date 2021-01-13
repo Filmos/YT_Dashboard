4de5afc5de078a202532013b178b5ccba271aa69
Filmos <filmosyt@gmail.com>
2021-01-13 12:20:22 +0100

    Fixed importing not working when launched from file

--- js/youtube-api.js
+++ js/youtube-api.js
@@ -1,11 +1,3 @@
-function loadAPIClient() {
-  gapi.client.setApiKey("AIzaSyAwPHCX3POkrRAeqqir-op4yTSgtxKyRbY");
-  return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-      .then(function() { console.log("GAPI client loaded for API"); },
-            function(err) { console.error("Error loading GAPI client for API", err); });
-}
-gapi.load('client', loadAPIClient);
-
 var _apiData = {}
 function getDataFromAPI(payload, onDelivered, onFailed, onSegment) {
   var fullPayload = payload.filter(function(id) {return !_apiData[id]})
@@ -24,7 +16,7 @@ function getDataFromAPI(payload, onDelivered, onFailed, onSegment) {
     executeAPIRequest()
   }
   function executeAPIRequest() {
-    var payload = fullPayload.slice(0, 50)
+    var payload = fullPayload.slice(0, 50).map(id => "&id="+id).join("")
     
     if(payload.length == 0) {
       payloadWorkers -= 1
@@ -33,44 +25,48 @@ function getDataFromAPI(payload, onDelivered, onFailed, onSegment) {
     }
     fullPayload = fullPayload.slice(50)
     
-    return gapi.client.youtube.videos.list({
-      "part": [
-        "snippet,contentDetails,statistics,status"
-      ],
-      "id": [
-        payload
-      ]
-    })
-    .then(function(response) {
-          response.result.items.forEach(function(vid) {
-            if(!vid.id || !vid.status) return
-            if(vid.status.privacyStatus != "public") return
-            if(!vid.status.publicStatsViewable) return
-            if(!vid.statistics.likeCount || !vid.statistics.dislikeCount || !vid.statistics.viewCount || !vid.statistics.commentCount) return
-            _apiData[vid.id] = {
-              likes: vid.statistics.likeCount,
-              dislikes: vid.statistics.dislikeCount,
-              views: vid.statistics.viewCount,
-              comments: vid.statistics.commentCount,
-              thumbnail: vid.snippet.thumbnails.high.url,
-              language: vid.snippet.defaultAudioLanguage,
-              category: vid.snippet.categoryId,
-              published: vid.snippet.publishedAt,
-              duration: vid.contentDetails.duration
-            }
-          })
-          counter+=1
-          onPayloadSegment(counter/segmentCount)
-          executeAPIRequest()
-        },
-        function(err) { 
-          console.error("Execute error", err.result.error); 
-          if(payloadWorkers > 0) {
-            payloadWorkers = 0
-            payload = []
-            onPayloadFailed(err.result.error)
-          }
-        });
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics%2Cstatus"+payload+"&key=AIzaSyAwPHCX3POkrRAeqqir-op4yTSgtxKyRbY", true);
+    xhr.onreadystatechange = function (aEvt) {
+      if (xhr.readyState == 4) {
+         if(xhr.status == 200)
+          parseResult(JSON.parse(xhr.response));
+         else
+          handleError(JSON.parse(xhr.response).error);
+      }
+    };
+    xhr.send(null); 
+    
+    function parseResult(response) {
+      response.items.forEach(function(vid) {
+        if(!vid.id || !vid.status) return
+        if(vid.status.privacyStatus != "public") return
+        if(!vid.status.publicStatsViewable) return
+        if(!vid.statistics.likeCount || !vid.statistics.dislikeCount || !vid.statistics.viewCount || !vid.statistics.commentCount) return
+        _apiData[vid.id] = {
+          likes: vid.statistics.likeCount,
+          dislikes: vid.statistics.dislikeCount,
+          views: vid.statistics.viewCount,
+          comments: vid.statistics.commentCount,
+          thumbnail: vid.snippet.thumbnails.high.url,
+          language: vid.snippet.defaultAudioLanguage,
+          category: vid.snippet.categoryId,
+          published: vid.snippet.publishedAt,
+          duration: vid.contentDetails.duration
+        }
+      })
+      counter+=1
+      onPayloadSegment(counter/segmentCount)
+      executeAPIRequest()
+    }
+    function handleError(err) { 
+      console.error("Execute error", err); 
+      if(payloadWorkers > 0) {
+        payloadWorkers = 0
+        fullPayload = []
+        onPayloadFailed(err)
+      }
+    }
   
 }
 
