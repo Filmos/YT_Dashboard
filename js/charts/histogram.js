7e3c1a01eab79bcc8d63ccd6702c21b3466a0751
ARUMHC <56126275+ARUMHC@users.noreply.github.com>
2021-01-15 23:17:20 +0100

    Add histogram for General

--- js/charts/histogram.js
+++ js/charts/histogram.js
@@ -1,14 +1,39 @@
-//const d3 = require("jquery");
 
 function updateHistogram(dataInput) {
 
-    var dataX = dataInput.map(v => v[SELECTED_DATATYPE]);
-    dataX.sort(function(a, b){return b-a});
-    //console.log("DATA X")
-    //console.log(dataX);
+    if (SELECTED_DATATYPE == "General"){
+        var data = d3.nest()
+            .key(function(v){
+                var datatmp = new Date(v.time) //v to jest to data input, time to data, datatmp to Data
+                datatmp.setHours(0)
+                datatmp.setMinutes(0)
+                datatmp.setSeconds(0)
+                datatmp.setMilliseconds(0)
+                return datatmp.getFullYear() + '-' + ('0' + (datatmp.getMonth()+1)).slice(-2) + '-' + ('0' + (datatmp.getDate()+1)).slice(-2)
+            })
+        data = data.rollup(v => v.length)
+        data = data.entries(dataInput)
+
+        //var dataFormat = "%Y-%m-%dT";
+
+        dataX=[]
+        for (const [key, value] of Object.entries(data)) {
+            dataX.push(value.value)
+        }
+        label = "Number of days"
+
+    }else{
+        var dataX = dataInput.map(v => v[SELECTED_DATATYPE]);
+        dataX.sort(function(a, b){return b-a});
+        label = 'Number of videos'
+
+    }
+
+    ////////
 
     min = d3.min(dataX);
     max = d3.max(dataX);
+
     domain = [min,max];
     Nbin = 15; // The number of bins
 
@@ -27,38 +52,42 @@ function updateHistogram(dataInput) {
         else {
             if (bucketsize != 0 || bucketborder > 1) {
                 buckets.unshift(bucketsize);
-                if (SELECTED_DATATYPE == "Like to dislike ratio"){
+                if (SELECTED_DATATYPE == "Like to dislike ratio") {
                     labels.unshift(bucketborder + "<");
-                }else {
+                } else {
                     labels.unshift(Math.round(bucketborder) + "<");
                 }
             }
             bucketsize = 0;
-            if (SELECTED_DATATYPE === "Likes per thousand views" || SELECTED_DATATYPE === "Dislikes per thousand views" || SELECTED_DATATYPE === "Comments per thousand views" ) {
+            if (SELECTED_DATATYPE === "Likes per thousand views" || SELECTED_DATATYPE === "Dislikes per thousand views" || SELECTED_DATATYPE === "Comments per thousand views") {
                 bucketborder = Math.round(bucketborder / 2);
-            }else if (SELECTED_DATATYPE === "Like to dislike ratio"){
+            } else if (SELECTED_DATATYPE === "Like to dislike ratio") {
                 bucketborder = bucketborder / 2;
-            //    console.log("druga")
-            //    bucketborder = bucketborder - step;
+                //    bucketborder = bucketborder - step;
+            } else if(SELECTED_DATATYPE == "General"){
+                //bucketborder = Math.round(bucketborder - step);
+                bucketborder = Math.round(bucketborder / 2);
+
             }else {
                 bucketborder = Math.round(bucketborder / 5);
             }
             //i = i - 1;
         }
     }
-    //buckets.unshift(bucketsize);
-    console.log(SELECTED_DATATYPE.toLowerCase());
-    console.log(min, max);
-    console.log(step)
-    console.log("BUCKET");
-    console.log(buckets);
-    console.log("LABELS");
-    console.log(labels);
+    //console.log(SELECTED_DATATYPE.toLowerCase());
+    //console.log(min, max);
+    //console.log(step)
+    //console.log("BUCKET");
+    //console.log(buckets);
+    //console.log("LABELS");
+    //console.log(labels);
 
 //end of data processing
+//graph generating
+
 
     if(SELECTED_DATATYPE == "General") {
-        $('#histogramTitle').text(" ")
+        $('#histogramTitle').text("Histogram of number of watched videos")
     } else {
         $('#histogramTitle').text("Histogram of number of" + SELECTED_DATATYPE.toLowerCase()+" over time")
     }
@@ -71,7 +100,7 @@ function updateHistogram(dataInput) {
         data: {
             labels: labels,
             datasets: [{
-                label: 'Number of videos',
+                label: label,
                 data: buckets,
                 backgroundColor: '#C34A36',
             }]
@@ -99,98 +128,5 @@ function updateHistogram(dataInput) {
             }
         }
     });
-
-
-    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-
-    /*
-    d3.select("#histogram>svg").remove();
-
-    var dataX = dataInput.map(v => v[SELECTED_DATATYPE]);
-
-    console.log("BLAHBLAH");
-
-    min = d3.min(dataX);
-    max = d3.max(dataX);
-    domain = [min,max];
-    Nbin = 10; // The number of bins
-
-
-    if(SELECTED_DATATYPE == "General") {
-        $('#histogramTitle').text(" ")
-    } else {
-        $('#histogramTitle').text("Histogram of " +SELECTED_DATATYPE.toLowerCase()+" over time")
-    }
-
-    var margin = { top: 30, right: 50, bottom: 30, left: 100 },
-        width = 1000 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-
-// The number of bins
-
-
-
-    var x = d3.scaleLinear()
-        .domain(domain)
-        .range([0, width]);
-
-
-    var histogram = d3.histogram()
-        .domain(x.domain()) // then the domain of the graphic
-        .thresholds(x.ticks(Nbin)); // theen the numbers of bins
-
-    //var histogram = d3.layout.histogram()
-    //    .bins(x.ticks(Nbin))
-    //    (dataX);
-
-// And apply this function to data to get the bins
-    var bins = histogram(dataX);
-
-    //console.log("pierwszy log");
-
-    var svg = d3.select("#histogram")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    //console.log("drugi log");
-
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    //console.log("trzeci");
-
-    var y = d3.scaleLinear()
-        .range([height, 0])
-        .domain([
-            0,
-            d3.max(bins, function(d) {
-                return d.length;
-            })
-        ]);
-
-    svg.append("g").call(d3.axisLeft(y));
-
-    svg
-        .selectAll("rect")
-        .data(bins)
-        .enter()
-        .append("rect")
-        .attr("x", 1)
-        .attr("transform", function(d) {
-            return "translate(" + x(d.x0) + "," + y(d.length) + ")";
-        })
-        .attr("width", function(d) {
-            return x(d.x1) - x(d.x0) - 1;
-        })
-        .attr("height", function(d) {
-            return height - y(d.length);
-        })
-        .style("fill", "#C34A36");
-
-     */
 
 }
